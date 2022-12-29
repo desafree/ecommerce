ecb7f4520ef8971a3e3042ae08c09447d5096e22
nicoladesanctis <nicoladesanctis99@gmail.com>
2022-12-29 15:44:12 +0100

    refactor: replace text with typography component

--- src/products/components/GoBack/GoBack.styled.tsx
+++ src/products/components/GoBack/GoBack.styled.tsx
@@ -4,12 +4,5 @@ export const GoBackStyled = styled.button`
   background: transparent;
   border: none;
   margin-bottom: 56px;
-  font-family: "Manrope";
-  color: rgba(0, 0, 0, 0.5);
-  font-size: 15px;
-  font-weight: 500;
-  line-height: 25px;
-  letter-spacing: 0px;
-  text-align: left;
   cursor: pointer;
 `;
--- src/products/components/GoBack/GoBack.tsx
+++ src/products/components/GoBack/GoBack.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { GoBackStyled } from "./GoBack.styled";
 import { useGoBack } from "../../hooks";
 import { useTranslate } from "../../../_shared";
+import { Typography } from "../../../_shared";
 
 export const GoBack = () => {
   const handleClick = useGoBack();
   const translate = useTranslate();
   return (
     <GoBackStyled onClick={handleClick}>
-      {translate("productAction.back")}
+      <Typography variant="smallBold">
+        {translate("productAction.back")}
+      </Typography>
     </GoBackStyled>
   );
 };
