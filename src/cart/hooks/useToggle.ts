07c48e4c2c8f8cf1a6822620b0e83095ecd7b083
nicoladesanctis <nicoladesanctis99@gmail.com>
2022-12-30 12:26:28 +0100

    refactor: change state update in useToggle

--- src/cart/hooks/useToggle.ts
+++ src/cart/hooks/useToggle.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export function useToggle() {
   const [on, setOn] = useState(false);
-  const toggle = () => setOn(!on);
+  const toggle = () => setOn((prevState) => !prevState);
 
   const onClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     e.stopPropagation();
