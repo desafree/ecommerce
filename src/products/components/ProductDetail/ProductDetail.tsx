48b893a0049edaaba73cdf6d06f701f50a382505
nicoladesanctis <nicoladesanctis99@gmail.com>
2022-12-30 10:22:43 +0100

    refactor: add translation go back in productdetail component

--- src/products/components/ProductDetail/ProductDetail.tsx
+++ src/products/components/ProductDetail/ProductDetail.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { ProductDetailStyled } from "./ProductDetail.styled";
 import { TextContainer } from "../TextContainer";
-import { Grid } from "../../../_shared";
+import { Grid, useTranslate } from "../../../_shared";
 import { Product } from "../../types";
 import { ImageContainer } from "../ImageContainer";
 import { Link } from "../../../_shared";
@@ -11,10 +11,12 @@ interface Props {
 }
 
 export const ProductDetail: FC<Props> = ({ productData }) => {
+  const translate = useTranslate();
+
   return (
     <ProductDetailStyled>
       <Link variant="goback" to={"/"}>
-        Go back
+        {translate("productAction.back")}
       </Link>
       <Grid container alignItems="center" justifyContent="center">
         <Grid item xs={12} md={6}>
